db376cafddf30335f8d701140111dea336cc7260
rafaelsouzars <rafael21.santos@hotmailcom>
2025-08-10 21:50:41 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ganchito
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
