e22a80ac43899af9955fba9f69de5b33d0698f3f
rafaelsouzars <rafael21.santos@hotmailcom>
2025-08-15 23:56:39 -0300

    :test_tube: test: Teste do Executer. Código inicial para demonstração da ferramenta. Author: rafaelsouzars.

--- Program.cs
+++ Program.cs
@@ -1,14 +1,39 @@
 ﻿using System;
 using ganchito.src.functions;
+using ganchito.Prompito;
+using ganchito.Prompito.ActionCommands;
 
 namespace ganchito
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Tela.Sobre("ganchito","v1.0.0","Utilitario para Git Hooks","https://github.com/rafasouzars","https://github.com/rafaelsouzars/ganchito");
-            Console.ReadLine();
+        {         
+            
+            // CONCLUÍDO...
+            var app = new Executer();
+
+            app.InsertAppData(
+                new {
+                    AppName = "",
+                    Version = "v1.0.0",
+                    Description = "Utilitário de Git Hook",
+                    ProfileURL = "https://github.com/rafasouzars",
+                    RepositorieURL = "https://github.com/rafaelsouzars/ganchito"
+                }); // 
+
+            
+            app.ScreenAbout(true);
+
+            app.AddCommand(
+                "init",
+                "Teste Filha inicia alguma coisa",
+                new ActionTeste()
+                );
+
+
+            app.ExecuteCommand(args);
+            
         }
     }
 }
